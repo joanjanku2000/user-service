8c58a997b274350abe115088de729c3dbf7eefae
Joan Janku <janku.joan4@gmail.com>
2023-05-03 00:19:35 +0200

    base service interface

--- src/main/java/com/onlinecv/userservice/base/service/BaseService.java
+++ src/main/java/com/onlinecv/userservice/base/service/BaseService.java
@@ -3,4 +3,8 @@ package com.onlinecv.userservice.base.service;
 import com.onlinecv.userservice.base.dto.BaseDTO;
 
 public interface BaseService<T extends BaseDTO> {
+    void save(T dto);
+    void update(T dto);
+    T findById(Long id);
+    void delete(Long id);
 }
