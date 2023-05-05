d2c06e72a5e0f9ad2c17ebb94960f56e48d68001
Joan Janku <janku.joan4@gmail.com>
2023-05-06 00:34:05 +0200

    fix @SqlDelete statement - requires db table name not jpql name

--- src/main/java/com/onlinecv/userservice/online_cv/model/entity/UserDataKey.java
+++ src/main/java/com/onlinecv/userservice/online_cv/model/entity/UserDataKey.java
@@ -2,6 +2,7 @@ package com.onlinecv.userservice.online_cv.model.entity;
 
 import com.onlinecv.userservice.base.entity.BaseEntity;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
 
@@ -9,7 +10,7 @@ import static com.onlinecv.userservice.base.entity.BaseEntity.DELETE_CLAUSE;
 
 @Entity
 @Where(clause = DELETE_CLAUSE)
-@SQLDelete(sql = "UPDATE UserDataKey u SET u.deleted = true where u.id = ?")
+@SQLDelete(sql = "UPDATE user_data_key u SET u.deleted = true where u.id = ?")
 public class UserDataKey extends BaseEntity {
     private String keyName;
 
